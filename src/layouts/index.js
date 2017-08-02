db8bc19c2fa67a3d7a9a16e43c15e7204aaa5390
wm3 <wm3@mail.w3ch.jp>
2017-08-03 02:29:49 +0900

    layouts/index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,45 +1,46 @@
-import React from "react"
-import { withRouter } from "react-router"
-import PropTypes from "prop-types"
-import Link from "gatsby-link"
-import Helmet from "react-helmet"
-import { lang } from "../utils/i18n.js"
+import React from 'react'
+import { withRouter } from 'react-router'
+import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+import { lang } from '../utils/i18n.js'
 
 export default withRouter(class Template extends React.Component {
   static propTypes = {
-    children: PropTypes.func,
+    children: PropTypes.func.isRequired,
+    location: PropTypes.object.isRequired
   }
 
-  render() {
+  render () {
     const thisLang = lang(this.props.location)
     return (
       <div lang={thisLang}>
         <Helmet
-          title="Gatsby Default Starter"
+          title='Gatsby Default Starter'
           meta={[
-            { name: "description", content: "Sample" },
-            { name: "keywords", content: "sample, something" },
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' }
           ]}
         />
         <div
           style={{
             background: `rebeccapurple`,
-            marginBottom: `1.45rem`,
+            marginBottom: `1.45rem`
           }}
         >
           <div
             style={{
               margin: `0 auto`,
               maxWidth: 960,
-              padding: `1.45rem 1.0875rem`,
+              padding: `1.45rem 1.0875rem`
             }}
           >
             <h1 style={{ margin: 0 }}>
               <Link
-                to="/"
+                to='/'
                 style={{
-                  color: "white",
-                  textDecoration: "none",
+                  color: 'white',
+                  textDecoration: 'none'
                 }}
               >
                 Gatsby
@@ -52,7 +53,7 @@ export default withRouter(class Template extends React.Component {
             margin: `0 auto`,
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
+            paddingTop: 0
           }}
         >
           {this.props.children()}

465510c1ab2ffadb75207ca597e657d72f5d818c
wm3 <wm3@mail.w3ch.jp>
2017-08-03 02:37:12 +0900

    pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,19 @@
-import React from "react"
-import Helmet from "react-helmet"
-import Link from "../utils/link"
-import GLink from "gatsby-link"
-import i18n from "../utils/i18n"
+import React from 'react'
+import Helmet from 'react-helmet'
+import Link from '../utils/link'
+import GLink from 'gatsby-link'
+import i18n from '../utils/i18n'
 
-
-export default function Index() {
+export default function Index () {
   return (
     <div>
-      <h2><Text id="intro" /></h2>
-      <Link to="/page-2/"><Text id="goToSessionList" /></Link><br/>
-      <GLink to="/">Japanese Top</GLink>&nbsp;
-      <GLink to="/en">English Top</GLink>
+      <h2><Text id='intro' /></h2>
+      <Link to='/page-2/'><Text id='goToSessionList' /></Link><br />
+      <GLink to='/'>Japanese Top</GLink>&nbsp;
+      <GLink to='/en'>English Top</GLink>
       <Helmet>
-      {//<link rel="css" href={CustomCss} />
-      }
+        {// <link rel="css" href={CustomCss} />
+        }
       </Helmet>
     </div>
   )
@@ -30,4 +29,4 @@ const Text = i18n({
     intro: `トップページです。`,
     goToSessionList: `セッションリストへ`
   }
-});
+})
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,41 +1,37 @@
-import React from "react"
-import Link from "../utils/link"
-import Helmet from "react-helmet"
-import i18n from "../utils/i18n"
+import React from 'react'
+import Link from '../utils/link'
+import i18n from '../utils/i18n'
 
-export default class Page2 extends React.Component {
-  render() {
-    const timetable = this.props.data.allTimetable.edges[0].node.sessions
+export default function page2 ({ data }) {
+  const timetable = data.allTimetable.edges[0].node.sessions
 
-    const items = timetable.map(s =>
-      <div key={`session-${s.id}`}>
+  const items = timetable.map(s =>
+    <div key={`session-${s.id}`}>
       <h4>{s.title}</h4>
       <p>{s.description}</p>
-      </div>
-    )
+    </div>
+  )
 
-    return (
-      <div>
-        <h1><Text id="title" /></h1>
-        {items}
-        <Link to="/">Go back to the homepage</Link>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1><Text id='title' /></h1>
+      {items}
+      <Link to='/'>Go back to the homepage</Link>
+    </div>
+  )
 }
 
 const Text = i18n({
   en: {
-    title: `Session`,
+    title: `Session`
   },
 
   ja: {
-    title: `セッション`,
+    title: `セッション`
   }
-});
+})
 
-
-export const pageQuery = graphql `
+export const pageQuery = graphql`
 query AllTimeline {
   allTimetable {
     edges {

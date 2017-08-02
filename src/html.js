af7d53dcc5cad91ce7c4dd657b88a16e3bc52d89
wm3 <wm3@mail.w3ch.jp>
2017-08-03 02:29:43 +0900

    html.js

--- src/html.js
+++ src/html.js
@@ -1,23 +1,24 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Helmet from "react-helmet"
+import React from 'react'
+import PropTypes from 'prop-types'
 
 export default class HTML extends React.Component {
   static propTypes = {
     body: PropTypes.string,
+    headComponents: PropTypes.element.isRequired,
+    postBodyComponents: PropTypes.element.isRequired
   }
 
-  render() {
+  render () {
     return (
       <html>
         <head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta charSet='utf-8' />
+          <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+          <meta name='viewport' content='width=device-width, initial-scale=1.0' />
           {this.props.headComponents}
         </head>
         <body>
-          <div id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
+          <div id='___gatsby' dangerouslySetInnerHTML={{ __html: this.props.body }} />
           {this.props.postBodyComponents}
         </body>
       </html>

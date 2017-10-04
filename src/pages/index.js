0c9b94281466164e8ac7e52a9471ca160998d988
wm3 <wm3@mail.w3ch.jp>
2017-10-04 20:12:45 +0900

    xxx

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,9 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import Link from '../utils/link'
-import GLink from 'gatsby-link'
-import i18n from '../utils/i18n'
 
 export default function Index () {
   return (
     <div>
-      <h2><Text id='intro' /></h2>
-      <Link to='/page-2/'><Text id='goToSessionList' /></Link><br />
-      <GLink to='/'>Japanese Top</GLink>&nbsp;
-      <GLink to='/en'>English Top</GLink>
       <Helmet>
         {// <link rel="css" href={CustomCss} />
         }
@@ -18,15 +11,3 @@ export default function Index () {
     </div>
   )
 }
-
-const Text = i18n({
-  en: {
-    intro: `Top Page.`,
-    goToSessionList: `Go to session list`
-  },
-
-  ja: {
-    intro: `トップページです。`,
-    goToSessionList: `セッションリストへ`
-  }
-})
